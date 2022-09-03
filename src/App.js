81cabf179687f3ca92946f96e24c7ca74803ec76
Dante Miller <dantemillerpersonal@gmail.com>
2022-09-02 21:37:36 -0500

    Add Background Image
    Added a background image that adjusts based on the screen size.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './sidebar';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import image from './image.jpeg';
 function App() {
   return (
     <Router>
-    <div className="App">
+    <div className="App" id="bg" style={{ 
+      backgroundImage: `url(${image})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundSize: "81%"
+     }}>
+      
     <Sidebar />
       
   
--- src/sidebar.js
+++ src/sidebar.js
@@ -13,8 +13,10 @@ const Sidebar = () => {
   return (
     <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
       <CDBSidebar textColor="#fff" backgroundColor="#333">
-        <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+        <CDBSidebarHeader>
           <a className="text-decoration-none" style={{ color: 'inherit' }}>
+
+            
             Dante Miller
           </a>
         </CDBSidebarHeader>
